4adf10917628f89650cb32a0764137d71e6fbf50
Yamil <currao.yamil@gmail.com>
2024-01-15 14:36:06 +0100

    Delete FundraisingInfo I don't use it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContributionSection from './components/ContributionSection';
-import FundraisingInfo from './components/FundraisingInfo';
 import ContributeForm from './components/ContributeForm';
 import HomeSection from './components/HomeSection';
 
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeSection />} />
           <Route path="/contribute" element={<ContributionSection />} />
-          <Route path="/fundraising-info" element={<FundraisingInfo />} />
           <Route path="/contribute-form" element={<ContributeForm />} />
         </Routes>
       </Router>
