bf2ba8044186fbe0fce2b624f0411df192bb2965
Yamil <currao.yamil@gmail.com>
2024-01-15 13:05:27 +0100

    Add a new Home section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContributionSection from './components/ContributionSection';
 import FundraisingInfo from './components/FundraisingInfo';
 import ContributeForm from './components/ContributeForm';
+import HomeSection from './components/HomeSection';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<h2>Home Section</h2>} />
+          <Route path="/" element={<HomeSection />} />
           <Route path="/contribute" element={<ContributionSection />} />
           <Route path="/fundraising-info" element={<FundraisingInfo />} />
           <Route path="/contribute-form" element={<ContributeForm />} />
---
+++ src/components/HomeSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeSection = () => {
+  return (
+    <div>
+      <h2>Home Section</h2>
+      {/* Add your content for the Home section */}
+    </div>
+  );
+};
+
+export default HomeSection;
