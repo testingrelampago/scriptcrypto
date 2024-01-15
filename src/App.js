9fbd11a287f953dde93ca05ffd924a2a0c94078b
Yamil <currao.yamil@gmail.com>
2024-01-15 15:20:55 +0100

    Add navigation menu to the website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ContributionSection from './components/ContributionSection';
-import ContributeForm from './components/ContributeForm';
 import HomeSection from './components/HomeSection';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        {/* Navigation Menu */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contribute">Contribute</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        {/* Routes */}
         <Routes>
           <Route path="/" element={<HomeSection />} />
           <Route path="/contribute" element={<ContributionSection />} />
-          <Route path="/contribute-form" element={<ContributeForm />} />
         </Routes>
       </Router>
     </div>
